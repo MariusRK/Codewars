cc0b559fbda61dc4e3b79e637e74b63739fecbcd
Marius Karlsen <marius-r-karlsen@outlook.com>
2020-03-05 00:05:39 +0100

    Updated with latest challenges

---
+++ Square(n)Sum/Kata.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Square_n_Sum
+{
+    class Kata
+    {
+        // Squares all numbers in an array and returns the sum of all. 
+        public static int SquareSum(int[] n)
+        {
+            int sum = 0;
+
+            foreach (int i in n)
+            {
+                sum += i * i;
+            }
+
+            return sum;
+        }
+
+        // Converts a number to string
+        public static string NumberToString(int num)
+        {
+            return num.ToString();
+        }
+
+        // Gets the middle character(s) of a string
+        public static string GetMiddle(string s)
+        {
+            if(s.Length == 1)
+            {
+                return s.ToString();
+            }
+            else if(s.Length % 2 == 0)
+            {
+                return s[s.Length / 2 - 1].ToString() + s[s.Length/2];
+            }
+            else
+            {
+                return s[s.Length / 2].ToString();
+            }
+        }
+    }
+}
---
+++ Square(n)Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Square_n_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SquareSum
+            // int[] numbers = { 1, 2, 2 };
+            // Console.WriteLine(Kata.SquareSum(numbers));
+
+            // NumberToString
+            // Console.WriteLine(Kata.NumberToString(1234));
+
+            // GetMiddle
+            // Console.WriteLine(Kata.GetMiddle("test"));
+            // Console.WriteLine(Kata.GetMiddle("testing"));
+            // Console.WriteLine(Kata.GetMiddle("middle"));
+            // Console.WriteLine(Kata.GetMiddle("a"));
+
+            Console.Read();
+        }
+    }
+}
